dee5e56c1fe7ecd93cb3d3f571e78c2ec35eb1f4
Daigoro Toyama <daigoro.toyama@raima.com>
2025-02-23 13:30:27 -0800

    Read a list of bad colours from a file

--- get_vim_color.sh
+++ get_vim_color.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+declare -a bad_color_list
+
+function create_bad_color_list ()
+{
+    local bad_color_file="$HOME/.vim/bad_colors.txt"
+
+    if [ -f "$bad_color_file" ]; then
+        local counter=0
+        while read -r line; do
+            bad_color_list[$counter]="$line"
+            counter=`expr $counter + 1`
+        done < $bad_color_file
+    fi
+}
+
+create_bad_color_list
+
 declare -a color_list
 
 # Return success if the specified colour is usable (i.e. dark)
 function use_this_color ()
 {
     local ret=0
-    local bad_color_list=("aqua" "calmbreeze" "default" "delek" "fine_blue" "morning" "olive" "shine" "tolerable" "zellner")
 
+    ## This works even if the list of bad colours is empty,
+    ## in which case the function returns success (0)
     for bad_color_name in "${bad_color_list[@]}"; do
         if [ "$1" == "$bad_color_name" ]; then
             #echo "Bad color ==> $1"
