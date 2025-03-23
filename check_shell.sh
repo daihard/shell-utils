042f66b80eddd17eee0e0b25f4701b790502fc0d
Daigoro Toyama <daigoro.toyama@raima.com>
2025-03-23 09:47:21 -0700

    Script to check the shell type

--- check_shell.sh
+++ check_shell.sh
@@ -3,5 +3,18 @@
 # Get the parent process name
 parent_shell=$(ps -o comm= -p $PPID)
 
-echo $parent_shell
-
+# Detect the shell
+case $parent_shell in
+    bash)
+        echo "Running in Bash."
+        ;;
+    zsh)
+        echo "Running in Zsh."
+        ;;
+    ksh)
+        echo "Running in KornShell (ksh)."
+        ;;
+    *)
+        echo "Running in an unknown or unsupported shell: $parent_shell"
+        ;;
+esac
