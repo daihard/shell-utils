ecdb665efee4b3cf2858798720dacba2608bb4e1
Daigoro Toyama <daigoro.toyama@raima.com>
2025-03-23 10:30:21 -0700

    Check shell script

--- check_shell.sh
+++ check_shell.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-# Get the parent process name
-parent_shell=$(ps -o comm= -p $PPID)
-
-# Detect the shell
-case $parent_shell in
-    bash)
-        echo "Running in Bash."
-        ;;
-    -zsh)
-        echo "Running in Zsh."
-        ;;
-    ksh)
-        echo "Running in KornShell (ksh)."
-        ;;
-    *)
-        echo "Running in an unknown or unsupported shell: $parent_shell"
-        ;;
-esac
+## check_shell() {
+##     # Detect the shell
+##     parent_shell=$(ps -o comm= -p $PPID)
+## 
+##     case $parent_shell in
+##         bash)
+##             echo "Bash"
+##             ;;
+##         -zsh)
+##             echo "Zsh"
+##             ;;
+##         *)
+##             echo "Unsuported"
+##             ;;
+##     esac
+## }
+## 
+#
+checkshell
+echo $1
