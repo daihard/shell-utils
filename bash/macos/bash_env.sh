e3079085bb34c3c5f448087c5d3fcf32c0b0961c
Daigoro Toyama <daigoro.toyama@raima.com>
2025-04-09 09:37:19 -0700

    Added a file description

--- bash/macos/bash_env.sh
+++ bash/macos/bash_env.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # vim: set filetype=sh:
 #
-# macos_env.sh
+# bash_env.sh
+# -----------
+# This file contains macOS-specific functions. Create a symlink (e.g.
+# ~/.macos_bash_env) so .bashrc can use it easily.
 
 # Fail if this file is used on a non-macOS platform
 [ "$(uname -s)" = "Darwin" ] || { echo "macOS required" >&2; exit 1; }
