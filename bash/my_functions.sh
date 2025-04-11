ba47baa8459412013206bd7318a41ed04949a780
Daigoro Toyama <daigoro.toyama@raima.com>
2025-04-11 09:52:18 -0700

    Updated ccat () to use gcat (GNU) if available

--- bash/my_functions.sh
+++ bash/my_functions.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # vim: set filetype=sh:
 
-
-myvim() {
+myvim () {
     if [[ -n "${DISPLAY:-}" ]] && command -v gvim >/dev/null 2>&1; then
         mygvim "$@"
     else
@@ -10,15 +9,22 @@ myvim() {
     fi
 }
 
-ccat() {
+ccat () {
+    # Use the GNU version if available (on macOS)
+    if command -v gcat >/dev/null 2>&1; then
+        cat_exec=gcat
+    else
+        cat_exec=cat
+    fi
+
     if command -v highlight >/dev/null 2>&1; then
         highlight -O ansi --force "$@"
     else
-        cat "$@"
+        ${cat_exec} "$@"
     fi
 }
 
-cless() {
+cless () {
     if command -v highlight >/dev/null 2>&1; then
         highlight -O ansi --force "$@" | \less -R
     else
@@ -30,6 +36,8 @@ alias vi='myvim'
 alias cat='ccat'
 alias less='cless'
 
+# bash_env.sh defines functions that are used both interactively
+# and in scripts.
 script_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")
 in_file=${script_dir}/bash_env.sh
 if [ -f $in_file ]; then
