cde1f45c212a229ac4d5922dd27d46c39d7dad88
Daigoro Toyama <daigoro.toyama@raima.com>
2025-04-11 12:49:26 -0700

    Added Linux-specific entries

--- bash/my_aliases.sh
+++ bash/my_aliases.sh
@@ -5,16 +5,22 @@
 alias cp='cp -ip'
 alias grep='grep -n'
 
-if [ "$(uname -s)" == "Darwin" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
     # These assume the following packages are installed via Homebrew
     # coreutils
     alias df='gdf'
     alias ls='gls --ignore=".DS_*" --group-directories-first --color=auto'
-    # alias ls='LC_COLLATE=C ls --color=auto'
     
     # findutils
     alias find='gfind'
     
     # gnu-sed
     alias sed='gsed'
+elif [ "$(uname -s)" = "Linux" ]; then
+    alias kt='kate'
+
+    # Fix the new annoying behaviour of ls...
+    unalias ls
+    ##alias ls='ls --color=auto --escape'
+    alias ls='LC_COLLATE=C ls --group-directories-first --color=auto'
 fi
