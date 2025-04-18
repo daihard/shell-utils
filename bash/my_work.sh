2884a88682e0c17c9afad69b9972935ff197d719
Daigoro Toyama <daigoro.toyama@raima.com>
2025-04-18 10:05:21 -0700

    Added my own git() function to force color outputs for 'git grep'

--- bash/my_work.sh
+++ bash/my_work.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+# my_work.sh
+
+# My own git function to force color outputs
+function git() {
+    if [[ "$1" = "grep" ]]; then
+        command git grep --color=always "${@:2}"
+    else
+        command git "$@"
+    fi
+}
 
 # Sverre's scripts
 function rcd {
@@ -11,11 +21,11 @@ function scd {
 
 script_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")
 
-if [ "$(uname -s)" == "Darwin" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
     in_file=${script_dir}/my_work_macos.sh
 elif [ "$(uname -s)" == "Linux" ]; then
     source /etc/os-release
-    if [ "$ID" == "ubuntu" ]; then
+    if [ "$ID" = "ubuntu" ]; then
         in_file=${script_dir}/my_work_ubuntu.sh
     else
         return 0
