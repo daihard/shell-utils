facc79f8c1f0edd94676bac43e555f2609eb068a
Daigoro Toyama <daigoro.toyama@raima.com>
2025-10-25 20:40:04 -0700

    Renaming script

--- rename.sh
+++ rename.sh
@@ -1,31 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-IFS='_'
-counter=0
+# Directory to scan (current directory by default)
+DIR="."
 
-declare -a ext_list=("jpg" "jpeg" "png" "JPG" "JPEG" "PNG")
+# Use first argument as prefix, default to "Dai"
+PREFIX="${1:-Dai}"
 
-for ext in ${ext_list[@]}; do 
-    found_file=$(find . -type f -name "*.${ext}")
-    if [[ "$found_file" != "" ]]; then
-        ## Convert each image file to a pdf
-        for f in $(ls -v *.${ext}); do
-            counter=0
-            for word in $f; do
-                counter=`expr $counter + 1`
-                echo $counter $word;
-                ##if [ $counter -gt 1 ]; then
-                ##    echo "$word";
-                ## fi
-            done
-##counter=0
-        done
-    fi
-done
+# Use second argument as starting number, default to 1
+START="${2:-1}"
 
-## for f in $(ls -v *.${ext}); do
-##     counter=`expr $counter + 1`
-## ##mv ./"$f" "$(printf "%0004d" $counter).jpg"
-##     echo "$f"
-## done
-
+# Find matching files and sort them by timestamp extracted from filename
+find "$DIR" -type f -name 'Screenshot *.png' | \
+  sed -E 's|.*/Screenshot ([0-9]{4})-([0-9]{2})-([0-9]{2}) at ([0-9]{2})\.([0-9]{2})\.([0-9]{2})\.png|\1\2\3\4\5\6 &|' | \
+  sort | \
+  cut -d' ' -f2- | \
+  awk -v dir="$DIR" -v prefix="$PREFIX" -v start="$START" 'BEGIN { count=start }
+  {
+    ext = substr($0, length($0)-3)
+    newname = sprintf("%s_%03d%s", prefix, count++, ext)
+    printf "mv \"%s\" \"%s/%s\"\n", $0, dir, newname
+  }' | bash
